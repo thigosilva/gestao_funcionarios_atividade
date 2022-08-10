004d50e26f4f5d8de58930db1386bef0cbe390b9
Gilvan Durães Galindo <gilvan.dgalindo@sp.senac.br>
2022-08-09 21:42:01 -0300

    first commit

---
+++ src/SENAC.OSA.TI.GerenciamentoFuncionario.AcessoDados/FuncionarioProvedorDados.cs
@@ -0,0 +1,34 @@
+﻿using SENAC.OSA.TI.GerenciamentoFuncionario.Comuns.Modelos;
+using SENAC.OSA.TI.GerenciamentoFuncionario.Comuns.ProvedorDados;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SENAC.OSA.TI.GerenciamentoFuncionario.AcessoDados
+{
+    public class FuncionarioProvedorDados : IFuncionarioProvedorDados
+    {
+        public IEnumerable<Cargo> CarregaCargos()
+        {
+            return new List<Cargo> {
+                new Cargo(1, "Desenvolvedor"),
+                new Cargo(2, "Engenheiro"),
+                new Cargo(3, "Arquiteto"),
+                new Cargo(4, "Gerente de Projetos")
+            };
+        }
+
+        public IEnumerable<Funcionario> CarregaFuncionarios()
+        {
+            return new List<Funcionario> {
+                new Funcionario(1, "Fulano de Tal", 1, false),
+                new Funcionario(2, "Ciclano de Tal", 2, true),
+                new Funcionario(3, "Beltrano de Tal", 3, true)
+            };
+        }
+
+        public void SalvaFuncionario(Funcionario funcionario)
+        {
+            Debug.WriteLine($"Funcionário salvo: {funcionario.PrimeiroNome}");
+        }
+    }
+}
---
+++ src/SENAC.OSA.TI.GerenciamentoFuncionario.Comuns/ProvedorDados/IFuncionarioProvedorDados.cs
@@ -0,0 +1,12 @@
+﻿using SENAC.OSA.TI.GerenciamentoFuncionario.Comuns.Modelos;
+using System.Collections.Generic;
+
+namespace SENAC.OSA.TI.GerenciamentoFuncionario.Comuns.ProvedorDados
+{
+    public interface IFuncionarioProvedorDados
+    {
+        IEnumerable<Funcionario> CarregaFuncionarios();
+        void SalvaFuncionario(Funcionario funcionario);
+        IEnumerable<Cargo> CarregaCargos();
+    }
+}
