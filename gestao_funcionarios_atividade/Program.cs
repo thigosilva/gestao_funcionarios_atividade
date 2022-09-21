e8f0031363ca456198d81ae2402bad06935c14fd
THIAGO SILVA ALEXANDRE <thiago.salexandre@senacsp.edu.br>
2022-09-21 00:25:52 -0300

    atualização

--- gestao_funcionarios_atividade/Program.cs
+++ gestao_funcionarios_atividade/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace gestao_funcionarios_atividade
 {
     class Program
     {
-        public Cargos(int id, string cargoNome)
-        {
-            id = id;
-            CargoNome = cargoNome;
-        }
+        private static object acessoii0;
 
-        public string CargoNome { get; }
+        public static object ListCEP { get; private set; }
+        public static object JsonConvert { get; private set; }
+
+        static async Task Main(string[] args)
+        {
+            string filePath = @"C:\Users\USUÁRIO\source\repos\Caso der Tudo errado";
+            var ObjetoDados = JsonConvert.SerializeObject(ListCEP);
+            await File.WriteAllTextAsync(filePath, ObjetoDados);
+            Console.WriteLine("Dados Salvos com Sucesso !!!");acessoii0
+        }
     }
 }
